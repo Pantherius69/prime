a1b4dd421d0a829d9ff9b119f9afcb0ed80ae44d
Pantherius69 <purusharthrai2006@gmail.com>
2022-02-25 10:51:31 +0530

    Initial commit

---
+++ main.java
@@ -0,0 +1,15 @@
+// prime
+
+import java.util.Scanner;
+
+class prime {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        // Write your code here
+
+        scanner.close();
+    }
+}
